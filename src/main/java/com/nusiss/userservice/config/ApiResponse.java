59b92f6a8ac6800eb5f1f3b58bb621ded88b5fa9
allenwuzhiwei <allenzhiwei0@gmail.com>
2025-08-07 11:41:17 +0800

    fixed apiresponse

--- src/main/java/com/nusiss/userservice/config/ApiResponse.java
+++ src/main/java/com/nusiss/userservice/config/ApiResponse.java
@@ -11,4 +11,17 @@ public class ApiResponse<T> {
     private boolean success;
     private String message;
     private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, "success", data);
+    }
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, "success", null);
+    }
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
 }
